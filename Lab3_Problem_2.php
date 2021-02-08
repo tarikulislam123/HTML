a48bee259170d9bc600d80d1432168a1939f0f13
tarikulislam123 <tarikulnayan12336@gmail.com>
2021-02-08 14:21:16 +0600

    Lab Task 3 Problem 02

---
+++ Lab3_Problem_2.php
@@ -0,0 +1,9 @@
+<?php
+	$marks=90;
+	if($marks>=90){echo "A+";}
+	elseif ($marks>80 & $marks<90){echo "A";}
+	elseif ($marks>70 & $marks<80){echo "A";}
+	elseif ($marks>60 & $marks<70){echo "A";}
+	else {echo "F";}
+	echo $marks;
+?>
